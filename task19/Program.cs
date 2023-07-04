ba806f80b869e10c280122a3b05dcc9822022eba
Oleg Yatmanov <o_yatmanov@mail.ru>
2023-07-04 15:49:07 +0500

    Домашнее задание 2 / семинар 5

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+string number = ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0] == number[4] || number[1] == number[3])
+  {
+    WriteLine($"{number} -> Да");
+  }
+  else WriteLine($"{number} -> Нет");
+}
+  CheckingNumber(number);
---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    Write($"{i * i * i}  ");
+}
+int ReadInt(string message)
+{
+    Write(message);
+    return int.Parse(ReadLine());
+}
